d572d8a99855c10b39b64bbc050110fe7307f6eb
Jonel Aron <jonel.villanueva@tecsup.edu.pe>
2026-08-31 06:01:01 -0500

    Estructura inicial del proyecto DesSolNubeTest

---
+++ conexion.php
@@ -0,0 +1,15 @@
+<?php
+
+    function conexion(){
+
+    $host = "host=dpg-da7fkduk1f9s73d8vikg-a.oregon-postgres.render.com";
+    $port = "port=5432";
+    $dbname = "dbname=test_db_3m0g";
+    $user = "user=test_db_3m0g_user";
+    $password = "0MOcF3uw6G4CFs2IVVR9YDpH3zGh9tkM";
+
+    $db = pg_connect("$host $port $dbname $user $password");
+
+    return $db;
+}
+?>
